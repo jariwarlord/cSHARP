10504a9469dd3635f5def49897986bc4ab70746f
Berke Dalar <57264829+jariwarlord@users.noreply.github.com>
2022-07-10 14:45:59 +0300

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE Kisiler
+
+CREATE TABLE Bilgi(
+id int primary key identity (1,1),
+kullanýcý_adi nvarchar(50),
+ad_soyad nvarchar(50),
+telefon nvarchar(15))
+
+
+SELECT * FROM Bilgi
