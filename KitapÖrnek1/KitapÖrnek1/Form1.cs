cfbc5b8b37b5aa73884121bf5c31f14abd1c2017
Berke Dalar <57264829+jariwarlord@users.noreply.github.com>
2022-07-12 15:14:25 +0300

    Add files via upload

---
+++ KitapÖrnek1/KitapÖrnek1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KitapÖrnek1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(textBox1.Text);
+            int sayi2 = Convert.ToInt32(textBox2.Text);
+            int sonuc = sayi1 + sayi2;
+            label1.Text = Convert.ToString(sonuc);
+        }
+    }
+}
